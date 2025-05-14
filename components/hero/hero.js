b2eb1744fb68bb8d0012cf0e6a63e69ff4544001
abdul ansari <samunleash1@yahoo.com>
2025-05-14 22:06:16 +0400

    added homepage hero image

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,55 +1,88 @@
-import React from 'react';
+import React from "react";
 import { Slide } from "react-awesome-reveal";
-import Link from 'next/link'
+import Link from "next/link";
 
 const Hero = () => {
+  const ClickHandler = () => {
+    window.scrollTo(10, 0);
+  };
 
-    const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
-
-    return (
-        // <section className="hero hero__style-one bg_img" style={{ backgroundImage: `url(${'/images/bg/hero_bg.jpg'})` }}>
-        <section className="hero hero__style-one bg_img" style={{ backgroundImage: `url(${'/images/bg/hero_banner_6.jpg'})` }}>
-            <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-xl-6 col-lg-7">
-                        <div className="hero__content">
-                            <Slide direction='up' triggerOnce={'false'} duration={1200}>
-                                {/* <h1>Immigration visa <br /> <span>Consulting</span></h1> */}
-                                <h1>Canada Immigration<br /> <span>via Express Entry & PNP</span></h1>
-                            </Slide>
-                            <Slide direction='up' triggerOnce={'false'} duration={1400}>
-                                {/* <p>Expert Guidance for a
+  return (
+    // <section className="hero hero__style-one bg_img" style={{ backgroundImage: `url(${'/images/bg/hero_bg.jpg'})` }}>
+    <section
+      className="hero hero__style-one bg_img"
+      style={{
+        backgroundSize: "cover",
+        // width: 1920,
+        // height: 755,
+        backgroundImage: `url(${"/images/bg/hero_bg.jpg"})`,
+      }}
+    >
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-xl-6 col-lg-7">
+            <div className="hero__content">
+              <Slide direction="up" triggerOnce={"false"} duration={1200}>
+                {/* <h1>Immigration visa <br /> <span>Consulting</span></h1> */}
+                <h1>
+                  Canada Immigration
+                  <br /> <span>via Express Entry & PNP</span>
+                </h1>
+              </Slide>
+              <Slide direction="up" triggerOnce={"false"} duration={1400}>
+                {/* <p>Expert Guidance for a
                                     Seamless Immigration Journey</p> */}
-                                <p>Immigration to Canada from UAE with expert support for PR, PNP & Express Entry.</p>
-                            </Slide>
-                            <ul className="xb-list list-unstyled mt-40 mb-60">
-                                <Slide direction='up' triggerOnce={'false'} duration={1600}>
-                                    {/* <li><i className="far fa-check"></i>Expert Legal Support</li>
+                <p>
+                  Immigration to Canada from UAE with expert support for PR, PNP
+                  & Express Entry.
+                </p>
+              </Slide>
+              <ul className="xb-list list-unstyled mt-40 mb-60">
+                <Slide direction="up" triggerOnce={"false"} duration={1600}>
+                  {/* <li><i className="far fa-check"></i>Expert Legal Support</li>
                                     <li><i className="far fa-check"></i>Meeting Your Unique Needs</li>
                                     <li><i className="far fa-check"></i>Tailored Immigration Solutions</li> */}
-                                    <li><i className="far fa-check"></i>Latest Canada Draws in 2024</li>
-                                    <li><i className="far fa-check"></i>Eligibility for Immigration to Canada</li>
-                                    <li><i className="far fa-check"></i>Canada PR process & requirements</li>
-                                </Slide>
-                            </ul>
-                            <Slide direction='up' triggerOnce={'false'} duration={1800}>
-                                <div>
-                                    <div className="btns">
-                                        {/* <Link onClick={ClickHandler} className="thm-btn" href="/contact">Book Appointment</Link>
+                  <li>
+                    <i className="far fa-check"></i>Latest Canada Draws in 2024
+                  </li>
+                  <li>
+                    <i className="far fa-check"></i>Eligibility for Immigration
+                    to Canada
+                  </li>
+                  <li>
+                    <i className="far fa-check"></i>Canada PR process &
+                    requirements
+                  </li>
+                </Slide>
+              </ul>
+              <Slide direction="up" triggerOnce={"false"} duration={1800}>
+                <div>
+                  <div className="btns">
+                    {/* <Link onClick={ClickHandler} className="thm-btn" href="/contact">Book Appointment</Link>
                                         <Link onClick={ClickHandler} className="thm-btn thm-btn--white" href="/about">Read Story</Link> */}
-                                        <Link onClick={ClickHandler} className="thm-btn" href="/contact">Book Appointment</Link>
-                                        <Link onClick={ClickHandler} className="thm-btn thm-btn--white" href="/about">Why Choose Canada Immigration?</Link>
-                                    </div>
-                                </div>
-                            </Slide>
-                        </div>
-                    </div>
+                    <Link
+                      onClick={ClickHandler}
+                      className="thm-btn"
+                      href="/contact"
+                    >
+                      Book Appointment
+                    </Link>
+                    <Link
+                      onClick={ClickHandler}
+                      className="thm-btn thm-btn--white"
+                      href="/about"
+                    >
+                      Why Choose Canada Immigration?
+                    </Link>
+                  </div>
                 </div>
+              </Slide>
             </div>
-        </section>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
