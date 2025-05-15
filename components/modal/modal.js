88869b1b71d82aa9faf04b2c0d2bb5ee5e329807
abdul ansari <samunleash1@yahoo.com>
2025-05-16 01:33:40 +0400

    added contact button and australia

---
+++ components/modal/modal.js
@@ -0,0 +1,85 @@
+import * as React from "react";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Fab from "@mui/material/Fab";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function TransitionsModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  // Open modal on page load
+  React.useEffect(() => {
+    handleOpen();
+  }, []);
+
+  return (
+    <Grid
+      sx={{
+        "@media (min-width: 300px) and (max-width: 900px)": {
+          position: "relative",
+        },
+        position: "fixed",
+        top: 370,
+        right: 0,
+        zIndex: " 9999",
+      }}
+    >
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "#00cc99",
+          writingMode: "vertical-rl",
+          textOrientation: "upright",
+          whiteSpace: "nowrap",
+          minWidth: "auto", // optional: keep it compact
+          padding: 1, // optional: adjust as needed
+        }}
+        onClick={handleOpen}
+      >
+        Contact Us
+      </Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </Grid>
+  );
+}
--- components/scrollbar/scrollbar.js
+++ components/scrollbar/scrollbar.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import dynamic from "next/dynamic";
+const NoSSR = dynamic(() => import("../modal/modal"), { ssr: false });
 
 const Scrollbar = () => {
-
-    return(
-        <div className="col-lg-12">
-            <div className="header-menu">
-                <ul className="smothscroll">
-                    <li><AnchorLink href='#__next'><i className="ti-arrow-up"></i></AnchorLink></li>
-                </ul>
-            </div>
-        </div>
-        
-    )
-}
+  return (
+    <div className="col-lg-12">
+      <div className="header-menu">
+        <NoSSR />
+        <ul className="smothscroll">
+          <li>
+            <AnchorLink href="#__next">
+              <i className="ti-arrow-up"></i>
+            </AnchorLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Scrollbar;
