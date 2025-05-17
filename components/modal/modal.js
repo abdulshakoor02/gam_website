a06436fb4fdfe9bd6ce85aa9eebd6208a9954219
abdul ansari <samunleash1@yahoo.com>
2025-05-17 16:31:02 +0400

    contact form changes

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -1,12 +1,18 @@
 import * as React from "react";
-import Backdrop from "@mui/material/Backdrop";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import Fade from "@mui/material/Fade";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import Fab from "@mui/material/Fab";
+import {
+  Card,
+  Grid,
+  Box,
+  Typography,
+  Button,
+  TextField,
+  MenuItem,
+  Modal,
+  Fade,
+  Backdrop,
+} from "@mui/material";
+import uuid from "react-uuid";
+import { useRouter } from "next/router";
 
 const style = {
   position: "absolute",
@@ -20,41 +26,34 @@ const style = {
   p: 4,
 };
 
-export default function TransitionsModal() {
+export default function TransitionsModal({
+  customStyle,
+  className,
+  modalStyle,
+  modalState,
+  buttonText,
+}) {
+  const router = useRouter();
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   // Open modal on page load
   React.useEffect(() => {
-    handleOpen();
+    if (modalState && router.pathname === "/") {
+      handleOpen();
+    }
   }, []);
 
   return (
-    <Grid
-      sx={{
-        "@media (min-width: 300px) and (max-width: 900px)": {
-          position: "relative",
-        },
-        position: "fixed",
-        top: 370,
-        right: 0,
-        zIndex: " 9999",
-      }}
-    >
+    <Grid sx={modalStyle}>
       <Button
         variant="contained"
-        sx={{
-          backgroundColor: "#00cc99",
-          writingMode: "vertical-rl",
-          textOrientation: "upright",
-          whiteSpace: "nowrap",
-          minWidth: "auto", // optional: keep it compact
-          padding: 1, // optional: adjust as needed
-        }}
+        className={className}
+        sx={customStyle}
         onClick={handleOpen}
       >
-        Contact Us
+        {buttonText}
       </Button>
       <Modal
         aria-labelledby="transition-modal-title"
@@ -70,14 +69,189 @@ export default function TransitionsModal() {
         }}
       >
         <Fade in={open}>
-          <Box sx={style}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-          </Box>
+          <Card sx={style}>
+            <Grid
+              sx={{
+                "& .MuiOutlinedInput-input": {
+                  borderColor: "white",
+                  color: "#9393a9",
+                },
+                "& .MuiInputLabel-root": {
+                  color: "#9393a9",
+                },
+                ".css-14s5rfu-MuiFormLabel-root-MuiInputLabel-root": {
+                  borderColor: "white",
+                  color: "#9393a9",
+                },
+                m: "5px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+                flexWrap: ["wrap", "nowrap"],
+              }}
+            >
+              <Typography
+                sx={{
+                  fontSize: "25px",
+                  fontWeight: "bold",
+                  color: "#147c67",
+                  mt: "20px",
+                }}
+              >
+                Free Assessment
+              </Typography>
+
+              <TextField
+                sx={{
+                  backgroundColor: "white",
+                  minWidth: "270px",
+                  color: "#147c67",
+                  mt: "10px",
+                }}
+                id={uuid()}
+                label="Full Name"
+                variant="outlined"
+              />
+              <TextField
+                sx={{
+                  backgroundColor: "white",
+                  minWidth: "270px",
+                  color: "#147c67",
+                  mt: "10px",
+                }}
+                id={uuid()}
+                label="Email"
+                variant="outlined"
+              />
+              <TextField
+                select
+                sx={{
+                  backgroundColor: "white",
+                  minWidth: "270px",
+                  color: "#147c67",
+                  mt: "10px",
+                }}
+                id={uuid()}
+                label="Destination"
+                variant="outlined"
+              >
+                <MenuItem key={uuid()} value="Canada">
+                  Canada
+                </MenuItem>
+                <MenuItem key={uuid()} value="Australia">
+                  Australia
+                </MenuItem>
+                <MenuItem key={uuid()} value="USA">
+                  USA
+                </MenuItem>
+                <MenuItem key={uuid()} value="New_Zealand">
+                  New Zealand
+                </MenuItem>
+                <MenuItem key={uuid()} value="Germany">
+                  Germany
+                </MenuItem>
+                <MenuItem key={uuid()} value="Denmark">
+                  Denmark
+                </MenuItem>
+                <MenuItem key={uuid()} value="Portugal">
+                  Portugal
+                </MenuItem>
+              </TextField>
+              <Grid>
+                <TextField
+                  sx={{
+                    backgroundColor: "white",
+                    color: "#147c67",
+                    mt: "10px",
+                    maxWidth: "90px",
+                    mt: "10px",
+                    mr: "2px",
+                  }}
+                  id={uuid()}
+                  label="Ext"
+                  variant="outlined"
+                />
+                <TextField
+                  sx={{
+                    backgroundColor: "white",
+                    borderColor: "white",
+                    maxWidth: "180px",
+                    color: "white",
+                    mt: "10px",
+                  }}
+                  id={uuid()}
+                  label="Mobile"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid>
+                <TextField
+                  select
+                  sx={{
+                    backgroundColor: "white",
+                    borderColor: "white",
+                    minWidth: "100px",
+                    color: "white",
+                    mt: "10px",
+                    mr: "2px",
+                  }}
+                  id={uuid()}
+                  label="Age"
+                  variant="outlined"
+                >
+                  <MenuItem key={uuid()} value="0-18">
+                    0-18 years
+                  </MenuItem>
+                  <MenuItem key={uuid()} value="18-45">
+                    18-45 years
+                  </MenuItem>
+                  <MenuItem key={uuid()} value="45+">
+                    45+
+                  </MenuItem>
+                </TextField>
+                <TextField
+                  select
+                  sx={{
+                    backgroundColor: "white",
+                    borderColor: "white",
+                    minWidth: "170px",
+                    color: "white",
+                    mt: "10px",
+                  }}
+                  id={uuid()}
+                  label="Education"
+                  variant="outlined"
+                >
+                  <MenuItem key={uuid()} value="Diploma">
+                    3 yrs Diploma
+                  </MenuItem>
+                  <MenuItem key={uuid()} value="Bachelors">
+                    Bachelors
+                  </MenuItem>
+                  <MenuItem key={uuid()} value="Masters">
+                    Masters
+                  </MenuItem>
+                  <MenuItem key={uuid()} value="Doctorate">
+                    Doctorate
+                  </MenuItem>
+                </TextField>
+              </Grid>
+              <Button
+                sx={{
+                  backgroundColor: "#00cc99",
+                  borderColor: "white",
+                  minWidth: "270px",
+                  color: "white",
+                  mt: "20px",
+                  mb: "40px",
+                }}
+                variant="contained"
+              >
+                Get Free Consultation
+              </Button>
+            </Grid>
+          </Card>
         </Fade>
       </Modal>
     </Grid>
