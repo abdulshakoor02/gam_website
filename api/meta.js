c6a0adb57f82e5cb9d7d5a4ee4fb690460f616ea
abdul ansari <samunleash1@yahoo.com>
2025-05-25 23:07:37 +0400

    meta desc and title added

---
+++ api/meta.js
@@ -0,0 +1,16 @@
+const meta = {
+  "/": {
+    title: "Best imigraion consultant in UAE",
+    desc: "We are one of the best immigration consultant in dubai",
+  },
+  "/canada/[slug]": {
+    title: "Best imigraion consultant in UAE for Canada",
+    desc: "We are one of the best immigration consultant in dubai",
+  },
+  "/australia/[slug]": {
+    title: "Best imigraion consultant in UAE for Canada",
+    desc: "We are one of the best immigration consultant in dubai",
+  },
+};
+
+export default meta;
